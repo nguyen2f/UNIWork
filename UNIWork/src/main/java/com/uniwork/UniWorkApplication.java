6e7b272d4b564077b21b3ebd82ff72ce01db8285
nguyen2f <nguyen.nguyenphuc.dev@gmail.com>
2025-04-26 21:43:44 +0700

    upload project

---
+++ UNIWork/src/main/java/com/uniwork/UniWorkApplication.java
@@ -0,0 +1,13 @@
+package com.uniwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniWorkApplication.class, args);
+	}
+
+}
---
+++ UNIWork/src/test/java/com/uniwork/UniWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
