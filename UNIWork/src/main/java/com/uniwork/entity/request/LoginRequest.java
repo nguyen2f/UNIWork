c51b1d4c9431af05cc15ed31b7da58a8b4be13f2
nguyen2f <nguyen.nguyenphuc.dev@gmail.com>
2025-10-09 11:54:55 +0700

    update v2: draft noti, file, comment

--- UNIWork/src/main/java/com/uniwork/entity/request/LoginRequest.java
+++ UNIWork/src/main/java/com/uniwork/entity/request/LoginRequest.java
@@ -1,4 +1,4 @@
-package com.uniwork.dto;
+package com.uniwork.entity.request;
 
 import lombok.Data;
 
--- UNIWork/src/main/java/com/uniwork/util/JwtUtil.java
+++ UNIWork/src/main/java/com/uniwork/util/JwtUtil.java
@@ -1,6 +1,6 @@
 package com.uniwork.util;
 
-import com.uniwork.model.User;
+import com.uniwork.entity.model.User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
